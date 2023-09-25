24dffe409f096520d6120599cefa942d77ca7961
Tate House <tatehouse100@gmail.com>
2023-09-25 19:47:47 -0400

    Feature: Two Sum

---
+++ Algorithms/TwoSum/TwoSum.cpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+        std::unordered_map<int, int> seen;
+
+        for (std::size_t index {0}; index < numbers.size(); ++index) {
+            const auto number {numbers[index]};
+            const auto complement {target - number};
+            const auto iterator {seen.find(complement)};
+
+            if (iterator != seen.end()) {
+                return {static_cast<int>(index), iterator->second};
+            }
+
+            seen.insert({number, static_cast<int>(index)});
+        }
+
+        throw std::invalid_argument("No two sum solution!");
+    }
+};

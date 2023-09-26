a5a8ad9ca6954353232363f92ce0aa0d2e51722a
Tate House <tatehouse100@gmail.com>
2023-09-25 20:53:35 -0400

    Feature: Palindrome Number

---
+++ Algorithms/PalindromeNumber/PalindromeNumber.cpp
@@ -0,0 +1,26 @@
+#include <limits>
+
+class Solution {
+public:
+    bool isPalindrome(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        const auto original {number};
+        auto reversed {0};
+
+        while (number > 0) {
+            const auto digit {number % 10};
+
+            if (reversed > (std::numeric_limits<int>::max() - digit) / 10) {
+                return false;
+            }
+
+            reversed = reversed * 10 + digit;
+            number = number / 10;
+        }
+
+        return original == reversed;
+    }
+};

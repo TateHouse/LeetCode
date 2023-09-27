27c5ca9a946b648e3d87abb41f8d67c3f4d02018
Tate House <tatehouse100@gmail.com>
2023-09-27 18:02:54 -0400

    Feature: Valid Parentheses

---
+++ Algorithms/ValidParentheses/ValidParentheses.cpp
@@ -0,0 +1,39 @@
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isValid(const std::string& string) {
+        if (string.size() % 2 != 0) {
+            return false;
+        }
+
+        const auto bracketPairs {std::unordered_map<char, char> {
+                {')', '('},
+                {']', '['},
+                {'}', '{'}
+        }};
+
+        auto stack {std::stack<char> {}};
+
+        for (const auto& character: string) {
+            const auto iterator {bracketPairs.find(character)};
+            if (iterator == bracketPairs.end()) {
+                stack.push(character);
+                continue;
+            }
+
+            const auto topElement {stack.empty() ? ' ' : stack.top()};
+            if (!stack.empty()) {
+                stack.pop();
+            }
+
+            if (topElement != iterator->second) {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+};

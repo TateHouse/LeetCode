5b8927664a06154a2ea945c145adaf2e23de0477
Tate House <tatehouse100@gmail.com>
2023-10-25 17:46:25 -0400

    Feature: Integer to Roman

---
+++ Algorithms/IntegerToRoman/IntegerToRoman.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <utility>
+
+class Solution {
+public:
+    std::string intToRoman(int number) {
+        const std::vector<std::pair<int, std::string>> romanNumerals {
+                {1000, "M"},
+                {900,  "CM"},
+                {500,  "D"},
+                {400,  "CD"},
+                {100,  "C"},
+                {90,   "XC"},
+                {50,   "L"},
+                {40,   "XL"},
+                {10,   "X"},
+                {9,    "IX"},
+                {5,    "V"},
+                {4,    "IV"},
+                {1,    "I"}
+        };
+
+        std::string result {};
+
+        for (const auto& [value, roman] : romanNumerals) {
+            while (number >= value) {
+                result.append(roman);
+                number = number - value;
+            }
+        }
+
+        return result;
+    }
+};

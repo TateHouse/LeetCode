ddbcc37bc8a1166f98218696485be986a18641f1
Tate House <tatehouse100@gmail.com>
2023-12-28 20:40:32 -0500

    Feature: Search Insert Position

---
+++ Source/SearchInsertPosition.cpp
@@ -0,0 +1,33 @@
+#include <numeric>
+#include <vector>
+
+namespace LeetCode {
+class Solution {
+public:
+    [[maybe_unused]] static const int searchInsert(const std::vector<int>& numbers, const int targetElement) {
+        auto leftIndex {0};
+        auto rightIndex {static_cast<int>(numbers.size())};
+
+        if (targetElement > numbers[rightIndex - 1]) {
+            return rightIndex;
+        }
+
+        while (leftIndex <= rightIndex) {
+            const auto middleIndex {std::midpoint(leftIndex, rightIndex)};
+            const auto& middleElement {numbers[middleIndex]};
+
+            if (middleElement == targetElement) {
+                return middleIndex;
+            }
+
+            if (middleElement > targetElement) {
+                rightIndex = middleIndex - 1;
+            } else {
+                leftIndex = middleIndex + 1;
+            }
+        }
+
+        return leftIndex;
+    }
+};
+}

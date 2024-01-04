5fbd8d75864337196fa544474f8a7e0a811b911a
Tate House <tatehouse100@gmail.com>
2024-01-04 15:11:09 -0500

    Feature: Valid Anagram

---
+++ Source/ValidAnagram.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <unordered_map>
+
+namespace LeetCode {
+class Solution final {
+public:
+    [[maybe_unused]] static const bool isValid(const std::string& first, const std::string& second) {
+        if (first.length() != second.length()) {
+            return false;
+        }
+
+        std::unordered_map<char, int> characters {};
+
+        for (const auto& character : first) {
+            characters[character]++;
+        }
+
+        for (const auto& character : second) {
+            if (!characters.contains(character)) {
+                return false;
+            }
+
+            auto& characterValue {characters[character]};
+            --characterValue;
+
+            if (characterValue == 0) {
+                characters.erase(character);
+            }
+        }
+
+        return characters.empty();
+    }
+};
+}

6f745486d829c1a8e95c5df60130590ea6784648
Tate House <tatehouse100@gmail.com>
2024-01-08 20:35:07 -0500

    Feature: K Closest Points to Origin

---
+++ Source/KClosestPointsToOrigin.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <vector>
+
+namespace LeetCode {
+class Solution final {
+public:
+    [[maybe_unused]] static const std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points,
+                                                                         const int closestPointCount) {
+        const auto comparison {[](const std::vector<int>& left, const std::vector<int>& right) {
+            const auto leftDistance {left[0] * left[0] + left[1] * left[1]};
+            const auto rightDistance {right[0] * right[0] + right[1] * right[1]};
+            return leftDistance < rightDistance;
+        }};
+
+        std::ranges::make_heap(points, comparison);
+
+        auto result {std::vector<std::vector<int>>(closestPointCount, std::vector<int>())};
+        for (auto index {0}; index < closestPointCount; ++index) {
+            std::ranges::pop_heap(points, comparison);
+            result[index] = std::move(points.back());
+            points.pop_back();
+        }
+
+        return result;
+    }
+};
+}

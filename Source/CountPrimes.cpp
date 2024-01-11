c100e801d55aa04381364fb08243dd1bc703fe77
Tate House <tatehouse100@gmail.com>
2024-01-10 21:45:59 -0500

    Feature: Count Primes

---
+++ Source/CountPrimes.cpp
@@ -0,0 +1,44 @@
+#include <bitset>
+#include <cmath>
+
+namespace LeetCode {
+class Solution final {
+public:
+    [[maybe_unused]] static const int getPrimeCount(const int number) {
+        if (number < 2) {
+            return 0;
+        }
+
+        std::bitset<5000001> isPrime {};
+        isPrime.set();
+        isPrime.reset(0);
+        isPrime.reset(1);
+
+        for (auto potentialPrime {2}; true; ++potentialPrime) {
+            const auto potentialPrimeSquared {static_cast<long long>(potentialPrime * potentialPrime)};
+
+            if (potentialPrimeSquared >= number) {
+                break;
+            }
+
+            if (!isPrime[potentialPrime]) {
+                continue;
+            }
+
+            for (auto multiple {potentialPrimeSquared}; multiple < number; multiple += potentialPrime) {
+                isPrime[multiple] = false;
+            }
+        }
+
+        auto primeCount {0};
+
+        for (auto index {2}; index < number; ++index) {
+            if (isPrime[index]) {
+                ++primeCount;
+            }
+        }
+
+        return primeCount;
+    }
+};
+}

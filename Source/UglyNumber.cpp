1eca6004bb007bc013eca2ab7d4cf421a890d795
Tate House <tatehouse100@gmail.com>
2024-01-11 17:29:21 -0500

    Feature: Ugly Number

---
+++ Source/UglyNumber.cpp
@@ -0,0 +1,20 @@
+#include <array>
+
+namespace LeetCode {
+class Solution final {
+public:
+    [[maybe_unused]] static const bool isNumberUgly(int number) {
+        if (number <= 0) {
+            return false;
+        }
+
+        for (constexpr std::array<int, 3> primeFactors {2, 3, 5}; const auto& primeFactor : primeFactors) {
+            while (number % primeFactor == 0) {
+                number /= primeFactor;
+            }
+        }
+
+        return number == 1;
+    }
+};
+}
